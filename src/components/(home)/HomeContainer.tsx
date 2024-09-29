8e6c1fe02fcd984a076fab892e40a0aa0b72f3d4
Palaksh dave <115778277+davepalaksh@users.noreply.github.com>
2024-09-30 01:33:03 +0530

    Update HomeContainer.tsx

--- src/components/(home)/HomeContainer.tsx
+++ src/components/(home)/HomeContainer.tsx
@@ -17,12 +17,12 @@ const HomeContainer: React.FC = () => {
         <div className="w-11/12 md:w-4/5 lg:w-3/4 xl:w-2/3 2xl:w-1/2 mx-auto">
             <div className="pb-8">
                 <div className="flex items-baseline">
-                    <h1 className="text-3xl font-medium text-zinc-100">Pranshu Patel</h1>
+                    <h1 className="text-3xl font-medium text-zinc-100">Palaksh Dave</h1>
                     <p className="text-sm px-1 text-zinc-400">(He/Him)</p>
                 </div>
-                <p className="text-zinc-400">{ageInYears}y/o Developer, India • {currentTime}</p>
+                <p className="text-zinc-400">Data engineer, India • {currentTime}</p>
             </div>
-            <p>Hey there! 👋🏻 I&apos;m Pranshu, a full-stack developer and uni sophomore based in India. Currently working on a few projects and pursuing B.Tech. in ICT from <a className="link" href="https://daiict.ac.in" target="_blank" rel="noopener noreferrer">DA-IICT</a>.</p>
+            <p>Hey there! 👋🏻 I&apos;m Palaksh, a Data Engineer based in India. 📫 Here's how you can reach me: <a className="link" href="https://mail.google.com/mail/u/0/" target="_blank" rel="noopener noreferrer">palakshdave@gmail.com</a>.</p>
             <SocialLinksGrid />
             <p>In my free time, I like to click photos, some of them are showcased below. Check out my <Link className='link' href='/gallery'>gallery</Link> for more. (Checkout my <a href='https://unsplash.com/@pranshu05' target='_blank' className='link'>Unsplash profile</a>.)</p>
             <HomeImageGrid />
