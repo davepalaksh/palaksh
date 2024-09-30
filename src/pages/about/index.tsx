aac1187b35471322d2675a3691c41616dd88ed1c
Palaksh dave <115778277+davepalaksh@users.noreply.github.com>
2024-09-30 11:51:58 +0530

    Update index.tsx

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -4,9 +4,9 @@ import Head from "next/head"
 const AboutPage: React.FC = () => (
     <>
         <Head>
-            <title>About | Pranshu05</title>
+            <title>About | Palaksh</title>
             <meta name="og:description" content="Portfolio website of Pranshu Patel." />
-            <meta name="og:title" content="Palaksh | Portfolio" />
+            <meta name="og:title" content="Pranshu05 | Portfolio" />
         </Head>
         <AboutContainer />
     </>
